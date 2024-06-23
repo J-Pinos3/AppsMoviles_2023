1d307ac873b68ce2000d2b9f801e928e11e29ca4
J-Pinos3 <josloco665@gmail.com>
2024-06-23 12:54:40 -0500

    C# basic stuff

---
+++ IntroductionCSharp/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace IntroductionCSharp
+{
+    class Class1
+    {
+
+        public static void Main()
+        {
+            int n = 5;
+
+            for (int i = 1; i <= n; i++)//5 rows
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    //Console.WriteLine(j + " "); //adds \n
+                    //Console.Write(j + " ");//does not add \n
+                    
+                }
+                Console.Write("\n");
+            }
+
+            for (int i = n-1;  i >= 1; i--){
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.Write("\n");
+            }
+
+            
+        }
+
+    }
+}
