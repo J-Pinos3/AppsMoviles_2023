80999ed9d00ce5115fc1b5668348a107bcf25b2a
J-Pinos3 <josloco665@gmail.com>
2024-10-23 22:40:08 -0500

    cookie clicker

---
+++ CookieClicker/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace CookieClicker
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private int counter = 0;
+        ImageButton ibCookie;
+        TextView tvClickCounter;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            ibCookie = FindViewById<ImageButton>(Resource.Id.ibCookie);
+            tvClickCounter = FindViewById<TextView>(Resource.Id.tvClickCounter);
+
+            ibCookie.Click += BtnIncreaseClickCounter;
+        }
+
+        private void BtnIncreaseClickCounter(object sender, EventArgs e)
+        {
+            counter++;
+            if (counter == 1)
+            {
+                tvClickCounter.Text = counter.ToString() + " click";
+            }
+            else
+            {
+                tvClickCounter.Text = counter.ToString() + " clicks";
+            }
+        }
+
+    }
+}
